fae5804728a7e66281061da3ae3c84f211810cc4
Willzey13 <89310144+Willzey13@users.noreply.github.com>
2024-12-12 11:33:53 -0300

    Adding WindowProperty.hx

--- src/flxwindowkit/FlxWindow.hx
+++ src/flxwindowkit/FlxWindow.hx
@@ -2,11 +2,14 @@
 
 package flxwindowkit;
 
+import flxwindowkit.cpp.WindowProperty.Border;
+
 class FlxWindow
 {
     public var width:Int;
     public var height:Int;
     public var title:String;
+    public var border:Border;
 
     // Construtor da janela
     public function new(width:Int, height:Int, title:String) 
@@ -15,6 +18,14 @@ class FlxWindow
         this.width = width;
         this.height = height;
         this.title = title;
+    }
+
+    public function setColorBorder(color:String = '')
+    {
+        if (['', ' '].contains(color))
+            return border.setBorderColor('white');
+        else
+            return border.setBorderColor(color);
     }
 
     // Método para exibir informações da janela
---
+++ src/flxwindowkit/cpp/WindowProperty.hx
@@ -0,0 +1,58 @@
+package cpp;
+
+
+
+#if windows
+@:buildXml('
+	<target id="haxe">
+		<lib name="dwmapi.lib" if="windows" />
+	</target>
+	')
+	@:cppFileCode('
+		#include <Windows.h>
+		#include <dwmapi.h>
+		#include <winuser.h>
+	')
+#elseif linux
+	@:cppFileCode("#include <stdio.h>")
+#end
+
+class Border
+{
+	public var color:String = 'dark';
+	public function setBorderColor(color:String):Void
+	{
+		this.color = color;
+		trace('Processing the color of the chosen border...');
+		
+		#if cpp
+			if (['dark', 'escuro', 'preto', 'black'].contains(color))
+			{
+				cppWindow.setWindowColorMode(true);
+				trace('Ready the color of the game window border has been changed to Black!!');
+			}
+			else if (['white', 'light', 'branco', 'claro'].contains(color))
+			{
+				cppWindow.setWindowColorMode(true);
+				trace('Ready the color of the game window border has been changed to White!!');
+			}
+		#end
+	}
+}
+
+class cppWindow
+{
+	public static function setWindowColorMode(isDarkMode:Bool) 
+	{
+		#if windows
+			untyped __cpp__('
+				int darkModeValue = isDarkMode ? 1 : 0;
+				HWND window = GetActiveWindow();
+				if (S_OK != DwmSetWindowAttribute(window, 19, &darkModeValue, sizeof(darkModeValue))) {
+					DwmSetWindowAttribute(window, 20, &darkModeValue, sizeof(darkModeValue));
+				}
+				UpdateWindow(window);
+			', isDarkMode); 
+		#end
+	}
+}
