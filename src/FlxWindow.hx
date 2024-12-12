48a50ed2d199aec1dcea731fc6e1645c47d0d15b
Willzey13 <89310144+Willzey13@users.noreply.github.com>
2024-12-12 10:59:35 -0300

    Create FlxWindow.hx

---
+++ src/FlxWindow.hx
@@ -0,0 +1,28 @@
+// flxwindowkit/Window.hx
+
+package;
+
+class FlxWindow
+{
+    public var width:Int;
+    public var height:Int;
+    public var title:String;
+
+    // Construtor da janela
+    public function new(width:Int, height:Int, title:String) 
+    {
+		super();
+        this.width = width;
+        this.height = height;
+        this.title = title;
+    }
+
+    // Método para exibir informações da janela
+    public function showInfo():Void 
+    {
+        trace("Window Info:");
+        trace("Title: " + this.title);
+        trace("Width: " + this.width);
+        trace("Height: " + this.height);
+    }
+}
